4510b13781c2487b7340bb65032e34fbc41f5ca9
angelleiva67 <158854371+angelleiva67@users.noreply.github.com>
2024-02-11 19:52:46 -0600

    Versión final del numero secreto

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Juego del numero secreto";  
+
+let parrrafo = document.querySelector('p');
+parrrafo.innerHTML = "indica un  numero del 1 al 10";
+
+function intententoDeusurio() {
+    alert('Clik en el boton');
+}
